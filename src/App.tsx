590d2180f9e834a2b6300dbc9ee288e7102ed3ad
Rafael <jiyuunotameni@hotmail.com>
2023-11-10 09:48:49 -0300

    Implementing Level and Roll

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState, useRef, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
-import { Capacities, Player } from './UserDomain'
+import { Capacities, Player, calculateLevelOfPlayer } from './UserDomain'
 import IdentityAndSummary from './components/IdentityAndSummary/IdentityAndSummary';
 import CapacitiesElement from './components/Attibutes/Capacities';
 import EditableText from './components/EditableText/EditableText';
@@ -15,14 +15,14 @@ import Moviments from './components/Moviments/Moviments';
 import Navbar, { AUTH_CODE, PLAYER_CODE, PLAYER_ID_CODE } from './components/Navbar/Navbar';
 import GetRecentCharacterService from './services/GetRecentCharacterService';
 import { ToastContainer } from 'react-toastify';
+import Roll from './components/Roll/Roll';
 
 
 
 function App() {
   const [user, setUser] = useState<Player>(BLANK_PLAYER)
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const jumpRefs: React.MutableRefObject<HTMLElement>[] = Array.from({ length: 9 }, () => useRef<HTMLElement>()) as React.MutableRefObject<HTMLElement>[]
-
+  const jumpRefs: React.MutableRefObject<HTMLElement>[] = Array.from({ length: 10 }, () => useRef<HTMLElement>()) as React.MutableRefObject<HTMLElement>[]
   const navigate = useNavigate()
 
   const mundainAtributes: Partial<Capacities> = useMemo(() => {
@@ -69,7 +69,10 @@ function App() {
         if(response){
           return response.data as Player
         }
-      } catch(e) { return undefined }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch(e: any) {
+        return undefined
+      }
     }
 
     requestUserFromServer()
@@ -117,34 +120,38 @@ function App() {
           dataSetter={(value: string) => setUser({ ...user, nickname: value })}
           className="players-nick-name small-hover"
         />
+        <p className='player-level'>{calculateLevelOfPlayer(user)}</p>
 
         <span ref={jumpRefs[1]}>
           <IdentityAndSummary user={user} setUser={setUser} />
         </span>
         <span ref={jumpRefs[2]}>
+          <Roll/>
+        </span>
+        <span ref={jumpRefs[3]}>
           <CapacitiesElement title='basics' baseAtributeData={mundainAtributes}
             user={user} setUser={setUser} setCapacities={(capa: Capacities) => setUser({...user, capacities: capa})}
           />
         </span>
-        <span ref={jumpRefs[3]}>
+        <span ref={jumpRefs[4]}>
           <CapacitiesElement title='specials' baseAtributeData={specialAtributes} className='special-container'
             user={user} setUser={setUser} setCapacities={(capa: Capacities) => setUser({...user, capacities: capa})}
             showAddNewAtributeButton={true}
           />
         </span>
-        <span ref={jumpRefs[4]}>
+        <span ref={jumpRefs[5]}>
           <Stats user={user} setUser={setUser} />
         </span>
-        <span ref={jumpRefs[5]}>
+        <span ref={jumpRefs[6]}>
           <Moviments user={user} setUser={setUser}/>
         </span>
-        <span ref={jumpRefs[6]}>
+        <span ref={jumpRefs[7]}>
           <EvolutionsAndExtensions user={user} setUser={setUser} />
         </span>
-        <span ref={jumpRefs[7]}>
+        <span ref={jumpRefs[8]}>
           <MinucesAndThings user={user} setUser={setUser} />
         </span>
-        <span ref={jumpRefs[8]}>
+        <span ref={jumpRefs[9]}>
           <Anotation user={user} setUser={setUser} />
         </span>
       </div>
