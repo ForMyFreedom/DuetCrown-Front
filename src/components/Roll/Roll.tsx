0d20a89a3396f3146503c61f65b6e4bed8eaff5d
Rafael <jiyuunotameni@hotmail.com>
2023-11-17 16:54:54 -0300

    Cifras Roll
    Also explanation of Advantage

--- src/components/Roll/Roll.tsx
+++ src/components/Roll/Roll.tsx
@@ -1,23 +1,33 @@
 import { useState } from 'react';
 import './Roll.css'
 
-const dices = [4, 6, 8, 10, 20, 100]
+const dices = [4, 6, 10, 20, 100]
 
 const Roll = () => {
   const [amount, setAmount] = useState<number[]>(Array(dices.length).fill(1))
-  const [result, setResult] = useState<number>(0)
+  const [result, setResult] = useState<string>('')
   const [showModal, setShowModal] = useState<boolean>(false)
+  const [cifraAmount, setCifraAmount] = useState<number>(1)
 
-  const rollDice = (dice: number, index: number) => {
-    const quant = amount[index]
+  const rollDice = (dice: number, quant: number) => {
+    if(!quant) { return }
     let result = 0
-
-    if(!quant) {
-      return
-    }
 
     for(let i = 0; i < quant; i++) {
       result += Math.floor(Math.random() * dice) + 1
+    }
+
+    setResult(String(result))
+    setShowModal(true)
+  }
+
+  const rollCifra = () => {
+    if(!cifraAmount) { return }
+    let result = ''
+    const options = ['👑', '⚜️']
+    
+    for(let i = 0; i < cifraAmount; i++) {
+      result += options[Math.floor(Math.random() * options.length)]
     }
 
     setResult(result)
@@ -37,23 +47,47 @@ const Roll = () => {
             <div key={dice} className='roll-dice'>
               <div style={{display: 'flex'}}>
                 <input value={amount[index] || 1}
-                 onChange={(event => setAmount(oldAmount => {
-                  const newAmount = [...oldAmount]
-                  newAmount[index] = Number(event.target.value)
-                  return newAmount
-                }))}
-                className='roll-input'/>
+                  onChange={(event => setAmount(oldAmount => {
+                    const newAmount = [...oldAmount]
+                    newAmount[index] = Number(event.target.value)
+                    return newAmount
+                  }))}
+                  className='roll-input'
+                />
                 <h3>d{dice}</h3>
               </div>
-              <button onClick={()=>rollDice(dice, index)}>Rolar</button>
+              <button onClick={()=>rollDice(dice, amount[index])}>Rolar</button>
             </div>
           ))}
+          <div key='cifra' className='roll-dice'>
+            <div style={{display: 'flex'}}>
+                <input value={cifraAmount}
+                  onChange={(event) => setCifraAmount(Number(event.target.value))}
+                  className='roll-input'
+                />
+                <h3>Cifra</h3>
+            </div>
+            <button onClick={rollCifra}>Rolar</button>
+          </div>
         </div>
+        <p className='topic--vantage-explanation'>
+          Jogar Cifras de Vantagem:
+        </p>
+        <p className='text--vantage-explanation'>
+          Para cada Coroa [👑], suba seu resultado (Falha -&gt; Sucesso Parcial -&gt; Sucesso Total)
+        </p>
+        <p className='topic--vantage-explanation'>
+          Jogar Cifras de Desvantagem:
+        </p>
+        <p className='text--vantage-explanation end--vantage-explanation'>
+          Para cada Revez [⚜️], desça seu resultado (Sucesso Total -&gt; Sucesso Parcial -&gt; Falha)
+        </p>
       </div>
       {showModal && (
         <div onClick={closeModal} className="modal">
           <div className="modal-content roll-modal">
-            <h2>Resultado: {result}</h2>
+            <h2>Resultado:</h2>
+            <h3>{result}</h3>
           </div>
         </div>
       )}
